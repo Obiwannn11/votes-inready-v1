b5594e25a8b508238f015445751a782d88545fff
Obiwannn11 <118538946+Obiwannn11@users.noreply.github.com>
2026-03-16 03:38:58 +0800

    refactor(home): migrate submission flow to dedicated route and clean up logic

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $submissionEvents = \App\Models\VotingEvent::where('status', 'submission_open')
-        ->latest()
-        ->get();
-    return view('voting.home', compact('submissionEvents'));
+    return view('voting.home');
 });
